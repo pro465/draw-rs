b41d11e22267b4a917a2248a48e977de429c2856
Proloy Mishra <67726964+pro465@users.noreply.github.com>
2021-11-02 17:04:54 +0530

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 use std::io::{self, Write};
 
 pub trait Draw {
-    type Args;
+    type Args1;
+    type Args2;
     type Ret;
 
-    fn draw(&self, writer: &mut impl Write) -> io::Result<Self::Ret>;
-    fn update(&mut self, _args: Self::Args) {}
+    fn draw(&self, writer: &mut impl Write, _args: Self::Args1) -> io::Result<Self::Ret>;
+    fn update(&mut self, _args: Self::Args2) {}
 }
